f3ad36a2a207dd0fb452f3b4bb29e3513e819bad
kanghye02 <115169462+kanghye02@users.noreply.github.com>
2023-04-26 22:11:11 +0700

    making function for AES.h
    This is an implementation of the AES algorithm, specifically ECB, CTR and CBC mode.
    Block size can be chosen in aes.h - available choices are AES128, AES192, AES256.

--- AES.c
+++ AES.c
@@ -1,6 +1,4 @@
 /*
-This is an implementation of the AES algorithm, specifically ECB, CTR and CBC mode.
-Block size can be chosen in aes.h - available choices are AES128, AES192, AES256.
 The implementation is verified against the test vectors in:
   National Institute of Standards and Technology Special Publication 800-38A 2001 ED
 ECB-AES128
